99203786c25159cc00f3ab7f5d57a96015762ee8
gandalf3 <zzyxpaw@gmail.com>
2015-02-26 04:05:01 -0800

    revert accidentally commited changes to uploadPage.php

--- Blend-exchangePHP/parts/uploadPage.php
+++ Blend-exchangePHP/parts/uploadPage.php
@@ -9,13 +9,14 @@
                     </div>
             </form>
             <div id="uploadOptions">
-                <input id="SEUrl" class="txtBlue bodyStack" <?php if(isset($embedUpload) && ($embedUpload == true)){echo 'style="display: none;"';}?> id="questionUrl" placeholder="Enter the url of the queston on blender.stackexchange" value="<?php if(isset($questionLink)) { echo $questionLink; }?>"/>
-                <div id="upload" class="btnBlue bodyStack" style="width: 289px; margin-left: 10px; display: inline-block;">
+                <input class="txtBlue bodyStack" <?php if(isset($embedUpload) && ($embedUpload == true)){echo 'style="display: none;"';}?> id="questionUrl" placeholder="Enter the url of the queston on blender.stackexchange" value="<?php if(isset($questionLink)) { echo $questionLink; }?>"/>
+                
+                <div>
+                    <input class="txtBlue bodyStack" id="password" placeholder="Enter a password to enable editing or deleting (optional)"/>
+                </div>
+                <div id="cancel" class="btnBlue bodyStack" style="width: 289px; display: inline-block;">
+                    Cancel
+                </div><div id="upload" class="btnBlue bodyStack" style="width: 289px; margin-left: 10px; display: inline-block;">
                     Upload
                 </div>
-            </div>
-            <div id="advancedOptions">
-               <div>
-                   <input class="txtBlue bodyStack" id="password" placeholder="Enter a password to enable editing or deleting (optional)"/>
-               </div>
             </div>
