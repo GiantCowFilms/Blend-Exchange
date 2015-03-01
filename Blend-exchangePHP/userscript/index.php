25c5abf52c02a30339f2b2a909880c1454d0d1f2
gandalf3 <zzyxpaw@gmail.com>
2015-03-01 01:37:46 -0800

    improve userscript page

--- Blend-exchangePHP/userscript/index.php
+++ Blend-exchangePHP/userscript/index.php
@@ -2,14 +2,19 @@
     <?php include("../parts/header.php"); ?>
         <div id="mainContainer">
             <h1>Userscript</h1>
-           <p> To use the userscript, please install <a href="https://tampermonkey.net/">Tampermonkey</a> or an equivalent plugin into your browser.</p>
+            <p>
+               We've written a userscript so you can upload files quickly, without ever leaving stackexchange.
+               It should work on Firefox (via <a href="https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/">Greasemonkey</a>) and Chrome (either as an extension or via <a href="https://tampermonkey.net/">Tampermonkey</a>).
+            </p>
+
             <div class="btnBlue"><a href="https://github.com/gandalf3/BlendExchanger/raw/master/blendexchanger.user.js">Download Now</a></div>
 
             <h2>See it in action:</h2>
-
             <img src="http://i.stack.imgur.com/26jbY.gif" alt="userscript in use">
-
-            <a href="http://meta.blender.stackexchange.com/a/634/3127">Learn more here</a>
+            
+            <p>
+               Request features or report problems on <a href="http://meta.blender.stackexchange.com/a/634/3127">stackexchange</a>.
+            </p>
         </div>
      <?php include("../parts/footer.php"); ?>
         <script src="jquery.js"></script>
