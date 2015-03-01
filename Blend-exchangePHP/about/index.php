4011b90e48360787b0911160327eafbce1060675
gandalf3 <zzyxpaw@gmail.com>
2015-03-01 00:25:29 -0800

    "userscript" seems to be more widely used than "user-script"

--- Blend-exchangePHP/about/index.php
+++ Blend-exchangePHP/about/index.php
@@ -9,8 +9,8 @@
             <h2>Why only blender.stackexchange?</h2>
             We have limited server space, and in order to have enough long into the future we need to limit the usage.
             <h2>Contributions</h2>
-                <a href="http://blender.stackexchange.com/users/3127/giantcowfilms">GiantCowFilms:</a> Server administration, lead development and design, and some assistance with the user-script<br />
-                <a href="http://blender.stackexchange.com/users/599/gandalf3">gandalf3:</a> Lead development on the user-script, minor development, editing, and the wonderful favicon.<br />
+                <a href="http://blender.stackexchange.com/users/3127/giantcowfilms">GiantCowFilms:</a> Server administration, lead development and design, and some assistance with the userscript<br />
+                <a href="http://blender.stackexchange.com/users/599/gandalf3">gandalf3:</a> Lead development on the userscript, minor development, editing, and the wonderful favicon.<br />
                 <a href="http://stackoverflow.com/users/201789/tehshrike">TehShrike:</a> Very helpful database wizard, wrote several database queries for the site.
             <h2>Who runs this?</h2>
             This crazy person:<br><br>
--- Blend-exchangePHP/userscript/index.php
+++ Blend-exchangePHP/userscript/index.php
@@ -2,12 +2,12 @@
     <?php include("../parts/header.php"); ?>
         <div id="mainContainer">
             <h1>Userscript</h1>
-           <p> To use the user-script, please install <a href="https://tampermonkey.net/">Tampermonkey</a> or an equivalent plugin into your browser.</p>
+           <p> To use the userscript, please install <a href="https://tampermonkey.net/">Tampermonkey</a> or an equivalent plugin into your browser.</p>
             <div class="btnBlue"><a href="https://github.com/gandalf3/BlendExchanger/raw/master/blendexchanger.user.js">Download Now</a></div>
 
             <h2>See it in action:</h2>
 
-            <img src="http://i.stack.imgur.com/26jbY.gif" alt="user-script in use">
+            <img src="http://i.stack.imgur.com/26jbY.gif" alt="userscript in use">
 
             <a href="http://meta.blender.stackexchange.com/a/634/3127">Learn more here</a>
         </div>
