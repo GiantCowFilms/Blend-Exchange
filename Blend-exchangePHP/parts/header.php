c0142d313956c66eff820c74034f5b38b66a6df6
gandalf3 <zzyxpaw@gmail.com>
2015-03-01 17:05:56 -0800

    make logo visible in tiny screen widths

--- Blend-exchangePHP/parts/header.php
+++ Blend-exchangePHP/parts/header.php
@@ -2,19 +2,25 @@
 <body>
    <div class="headspace"></div>
    <div class="wrapper"> <!-- needed for sticky footer -->
-   <div id="nav">
+   <div id="header">
       <div id="logo">
          <a href="/">
+         <div class="logoText">
             <span class="logoOrange">Blend</span><span class="logoBlue">-Exchange</span></a>
-            <div><a class="logoCredit" href="http://blender.stackexchange.com/users/">A long term .blend storage solution for blender.stackexchange</a></div>
             <div class="logoStatus">&#945;lpha</div>
-            <img src="/logo.png" />
+         </div>
+         <div>
+            <a class="logoCredit" href="http://blender.stackexchange.com/users/">A long term .blend storage solution for blender.stackexchange</a>
+         </div>
+         <img src="/logo.png" />
       </div>
-         <ul id="navLinks">
-            <li><a href="/">Upload</a></li>
-            <li><a href="/help">Help</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/userscript">Userscript</a></li>
-            <li><a href="/contribute">Contribute</a></li>
-         </ul>
+      <div id="nav">
+            <ul id="navLinks">
+               <li><a href="/">Upload</a></li>
+               <li><a href="/help">Help</a></li>
+               <li><a href="/about">About</a></li>
+               <li><a href="/userscript">Userscript</a></li>
+               <li><a href="/contribute">Contribute</a></li>
+            </ul>
+      </div>
    </div>
