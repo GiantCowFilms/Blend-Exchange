8bc2894b3ca0b0f462d137bc32afbf62f86821b5
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-06-08 19:14:11 -0700

    Update

--- Blend-exchangePHP/parts/checkLogin.php
+++ Blend-exchangePHP/parts/checkLogin.php
@@ -3,9 +3,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_set_cookie_params(86400);
     session_start();
-
+    session_regenerate_id();
 }
-session_regenerate_id();
 $loggedIn = false;
 $admin = false;
 if((isset($_SESSION["loggedIn"]) == true) && ($_SESSION["loggedIn"] == true)){
