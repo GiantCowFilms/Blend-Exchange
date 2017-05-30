3b9886b82648531812dde6ea8cbf14fe42fd8f9a
GiantCowFilms <git@GiantCowFilms.com>
2017-05-30 10:10:51 -0700

    Added code to ensure long blend names NEVER overflow

--- Blend-exchangePHP/parts/downloadPage.php
+++ Blend-exchangePHP/parts/downloadPage.php
@@ -50,7 +50,7 @@
                         <img class="blendDisplayIcon" src="/blenderFileIcon.png"/>
                         <div class="blendDisplayContainer" style="display: inline-block; margin-top: 25px; text-align: left;">
                             <h2 class="blendDisplayTitle">
-                                <?php echo $blendData["fileName"] ?>
+                                <?php  echo (strlen($blendData["fileName"]) > 50) ? substr($blendData["fileName"], 0, 50) . '... .' . pathinfo($blendData["fileName"], PATHINFO_EXTENSION) : $blendData["fileName"];  ?>
                             </h2>
                             <span class="downloadQuestionLink">
                                  <a href="<?php echo $blendData["questionLink"] ?>">View Question</a>
