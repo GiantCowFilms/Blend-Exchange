2720cb263ad5cf9de25b8dded32322697279cb01
GiantCowFilms <GiantCowFilms@users.noreply.github.com>
2018-10-21 01:06:47 -0700

    Added Test to make sure update is secured against guest user.

---
+++ tests/Integration/User/UpdateUserCest.php
@@ -0,0 +1,14 @@
+<?php namespace User;
+
+
+class UpdateUserCest
+{
+    public function unauthenticatedUserCannotEditUser(ApiTester $I) {
+        $firstUserId = $I->grabFromDatabase('users', 'id');
+        $I->sendPOST('/api/users/' . $firstUserId . '/update', [
+            'username' => 'Mallory',
+            'email' => 'mallory@example.com'
+        ]);
+        $I->seeResponseCodeIs(404);
+    }
+}
