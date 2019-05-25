71433403cd15cabc68fee45631e51290e0221e9c
GiantCowFilms <GiantCowFilms@users.noreply.github.com>
2019-05-25 11:30:22 -0700

    hotfix - don't rename users table if it doesn't already exist

--- migrations/20180609053005_create_users_table.php
+++ migrations/20180609053005_create_users_table.php
@@ -28,8 +28,11 @@ class CreateUsersTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('users');
-        $table->rename('legacy_users');
-        $table->update();
+        $exists = $this->hasTable('users');
+        if ($exists) {
+            $table->rename('legacy_users');
+            $table->update();
+        }
         $table = $this->table('users',['id' => false, 'primary_key' => 'id']);
         $table->addColumn('id','string',['limit' => 10]);
         $table->addColumn('stackId','string',['limit' => 50]); //Technically an integer
