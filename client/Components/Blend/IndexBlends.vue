7e99fdf3b5631ce33c61328cc98c3a5d3eb60f83
GiantCowFilms <GiantCowFilms@users.noreply.github.com>
2019-05-31 00:53:05 -0700

    Fix race conditionthat would cause all blends to be loaded rather than user blends.

--- client/Components/Blend/IndexBlends.vue
+++ client/Components/Blend/IndexBlends.vue
@@ -2,12 +2,14 @@
     <div>
         <pagination :query="query" :pagination="pagination"></pagination>
         <spinner v-if="loading" :width="'64px'"></spinner>
-        <div v-if="!loading" v-for="blend in blends" :key="blend.id" class="listItem">
-            <router-link :to="`/b/${blend.id}`">
-            <h2>{{ blend.fileName }}</h2>
-            </router-link>
-            <a :href="blend.questionLink">Question</a> | {{ blend.views_count }} views | {{blend.downloads_count }} downloads | {{ blend.favorites_count }} favorites
-        </div>
+        <template  v-else>
+          <div v-for="blend in blends" :key="blend.id" class="listItem">
+              <router-link :to="`/b/${blend.id}`">
+              <h2>{{ blend.fileName }}</h2>
+              </router-link>
+              <a :href="blend.questionLink">Question</a> | {{ blend.views_count }} views | {{blend.downloads_count }} downloads | {{ blend.favorites_count }} favorites
+          </div>
+        </template>
     </div>
 </template>
 <script>
@@ -28,7 +30,8 @@ export default {
     return {
       blends: [],
       pagination: {},
-      loading: false
+      loading: false,
+      lastRequest: undefined
     };
   },
   watch: {
@@ -45,12 +48,16 @@ export default {
   methods: {
     updateBlends: async function(query) {
       this.loading = true;
+      const requestId = Symbol();
+      this.$data.lastRequest = requestId;
       var response = await blendExchange.getEndpoint(`/blends`, {
         params: query
       });
-      this.loading = false;
-      this.$data.blends = response.data;
-      this.$data.pagination = response.meta.pagination;
+      if (this.$data.lastRequest === requestId) {
+        this.loading = false;
+        this.$data.blends = response.data;
+        this.$data.pagination = response.meta.pagination;
+      }
     }
   }
 };
